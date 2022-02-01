842d59466340ff4e0afe79290627607183dad013
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-02-01 17:41:30 +0100

    JS

--- modal.js
+++ modal.js
@@ -1,16 +1,67 @@
 // Test
-
 var a = 20,
   b = 4,
   result;
 
 
-
 // Go code
 
 const inputs = document.querySelectorAll(
-  'input[type="text-control"], input[type="password"]'
+  'input[type="text"], input[type="password"], input[type = "email"], input[type = "date"], input[type = "number"], input[type = "radio"], input[type = "checkbox"]'
 );
+
+
+// Fonctions controleurs check
+const firstChecker = () => {};
+const lastChecker = () => {};
+const emailChecker = () => {};
+const birthdateChecker = () => {};
+const numberChecker = () => {};
+const radioChecker = () => {};
+const checkboxChecker = () => {};
+
+
+
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    switch (e.target.value) {
+      case "first":
+        firstChecker()
+        break;
+
+      case "last":
+        firstChecker()
+        break;
+
+      case "email":
+        emailChecker()
+        break
+
+      case "birthdate":
+        emailChecker()
+        break
+
+      case "number":
+        emailChecker()
+        break
+
+      case "radio":
+        emailChecker()
+        break
+
+      case "radio":
+        checkboxChecker()
+        break
+
+      default:
+        nul;
+    }
+  });
+})
+
+
+
 
 
 
