1283d35f836e530c18183805a8fb2e0953a2fbf7
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-02-03 16:16:25 +0100

    txt vers txt.js

--- txt.js
+++ txt.js
@@ -1,13 +1,12 @@
-
 // Start code
 
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
 // DOM Elements
@@ -23,10 +22,10 @@ modalclose.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
 // close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+    modalbg.style.display = "none";
 }
