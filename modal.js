3acfd4e04bd9ce82e269530e4fe071d2b49c40d6
Matthieu Lombardia <85737628+matthieu-lombardia@users.noreply.github.com>
2022-02-03 16:21:34 +0100

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,3 +1,34 @@
+// Start code
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalclose = document.querySelector(".close");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalclose.addEventListener("click", closeModal);
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 // Variable déclarée Envoi formulaire
 const form = document.querySelectorAll("form")
 
